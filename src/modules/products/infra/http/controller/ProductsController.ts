ea0be386b0b5a08633ef0b47eafefd8ee8cc4f9d
Tales Garcia <garcia.tales@gmail.com>
2021-02-07 19:46:33 -0300

    feat(products controller): implement create method

--- src/modules/products/infra/http/controller/ProductsController.ts
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -5,6 +5,10 @@ import CreateProductService from '@modules/products/services/CreateProductServic
 
 export default class ProductsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, price, quantity } = request.body;
+
+    const product = await container.resolve(CreateProductService).execute({ name, price, quantity });
+
+    return response.status(201).json(product);
   }
 }
