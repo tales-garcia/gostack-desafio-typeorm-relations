36feb42ee484e0019dfbf25c7d70340b439d4155
Tales Garcia <garcia.tales@gmail.com>
2021-02-07 19:41:57 -0300

    feat(product entity): declare entity and columns

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -9,19 +9,21 @@ import {
 
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity()
 class Product {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
-
+  @Column()
   name: string;
-
+  @Column('decimal')
   price: number;
-
+  @Column('int')
   quantity: number;
-
+  @OneToMany(() => OrdersProducts, order_products => order_products.product)
   order_products: OrdersProducts[];
-
+  @CreateDateColumn()
   created_at: Date;
-
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
